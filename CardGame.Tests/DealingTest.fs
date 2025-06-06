6d5b30cee935b467cf8a27ece8a5074464beedbf
Michael Kargl <michael.kargl@zuehlke.com>
2025-06-06 16:15:27 +0200

    feat: init

---
+++ CardGame.Tests/DealingTest.fs
@@ -0,0 +1,28 @@
+﻿module DealingTest
+
+open CardGame.Cards
+open Xunit
+
+[<Fact>]
+let ``Should return 51 cards when dealing a card from a new deck`` () =
+    // Arrange
+    let expected = 52 - 1
+    let deck: Deck = newDeck
+    // Act
+    let shuffledDeck: ShuffledDeck = ShuffledDeck deck
+    let (newDeck, _) = dealCard shuffledDeck
+    let actual = newDeck |> countCardsInShuffledDeck
+    // Assert
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Should return 0 cards when dealing a card from a depleted deck`` () =
+    // Arrange
+    let expected = 0
+    // Act
+    let deck = ShuffledDeck []
+    let newDeck,_ = dealCard deck
+    let actual = newDeck |> countCardsInShuffledDeck
+    // Assert
+    Assert.Equal(expected, actual)
+
---
+++ CardGame/Program.fs
@@ -0,0 +1,33 @@
+﻿module CardGame.Program
+
+open System
+open CardGame.Cards
+
+type ErrorCodes =
+    static member public Success = 0
+    static member public UnhandledError = 1
+
+[<EntryPoint>]
+let main args : int =
+    try
+        let seed = Random.Shared.Next()
+        let mutable deck: ShuffledDeck = Cards.newDeck |> shuffle 123
+
+        while (true) do
+            Console.WriteLine("Draw a card (Enter/Ctrl+C)?")
+            Console.ReadKey() |> ignore
+            let (newDeck, card) = dealCard deck
+
+            let unpackedNewDeck = newDeck |> unpackDeck
+            printCard card
+
+            printfn "Next up: "
+            printDeckOverview 5 unpackedNewDeck
+            printfn "\n"
+
+            deck <- newDeck
+
+        ErrorCodes.Success
+    with ex ->
+        printfn $"An unknown exception has been thrown: {ex.Message}"
+        ErrorCodes.UnhandledError
